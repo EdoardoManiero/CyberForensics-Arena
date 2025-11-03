f76f3c345b8f566ab68531cd19f8befa48a1f1c8
edoardo maniero <edoardomaniero@gmail.com>
2025-11-03 18:37:02 +0100

    fixed enviroment import

--- js/scene.js
+++ js/scene.js
@@ -18,7 +18,7 @@ import { eventBus, Events } from './eventBus.js';
 // ============================================================================
 
 const SCENE_CONFIG = {
-  MODEL_PATH: './public/models/',
+  MODEL_PATH: './models/',
   DEFAULT_MODEL: 'secret_lab.glb',
   AMBIENT_INTENSITY: 0.9,
   CAMERA_START_POS: new BABYLON.Vector3(0, 1.6, 1.5),
